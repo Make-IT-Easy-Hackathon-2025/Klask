727d3018d2904aec3efe7bd1342d27ee3cb481d5
KisBenjamin <kis.beniamin.tibor@student.ms.sapientia.ro>
2025-02-28 22:28:40 +0200

    [KLASK-01]: landing page and theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -4,17 +4,19 @@ import { createTheme } from "@mui/material/styles";
 export const lightTheme = createTheme({
   palette: {
     mode: "light",
-    primary: { main: "#1976d2" },
-    secondary: { main: "#dc004e" },
-    background: { default: "#f5f5f5", paper: "#ffffff" },
+    primary: { main: "#FFCF50" }, // Light mode primary color
+    secondary: { main: "#A4B465" }, // Light mode secondary color
+    background: { default: "#626F47", paper: "#9aa87d" }, // Light mode background
+    text: { primary: "#FEFAE0", secondary: "#FEFAE0" }, // Light mode text
   },
 });
 
 export const darkTheme = createTheme({
   palette: {
     mode: "dark",
-    primary: { main: "#90caf9" },
-    secondary: { main: "#f48fb1" },
-    background: { default: "#121212", paper: "#1e1e1e" },
+    primary: { main: "#A27B5C" }, // Dark mode primary color
+    secondary: { main: "#3F4F44" }, // Dark mode secondary color
+    background: { default: "#2C3930", paper: "#1E1E1E" }, // Dark mode background
+    text: { primary: "#DCD7C9", secondary: "#DCD7C9" }, // Dark mode text
   },
 });
