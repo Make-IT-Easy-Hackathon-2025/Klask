4573054441e10680107827f72275ab9c050c73a9
NagoDaniel <nago.daniel@student.ms.sapientia.ro>
2025-03-01 10:35:57 +0200

    Notification model, getuser email id

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/userController";
+import { createUser, getUserById, getUserIdByEmail } from "../controllers/userController";
 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.get("/email/:email",getUserIdByEmail);
+router.get("/:id",getUserById);
 
 export default router;
